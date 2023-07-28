bfb0846cb341dc95308afae6a0512ba3e720a961
Melih Gunduz <melih.gunduz@hotmail.com>
2023-07-28 12:19:33 +0300

    Starter project created

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+
+const hre = require("hardhat");
+
+async function main() {
+
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Balance:", (await deployer.getBalance()).toString());
+
+  const bank = await hre.ethers.getContractFactory("Bank");
+  const bankContract = await bank.deploy();
+
+  await bankContract.deployed();
+
+  console.log("Bank deployed to:", bankContract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
